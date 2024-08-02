ffc8e7953e58011bc68446d8f46f991e46658f88
karmicharsh <harshworld002@gmail.com>
2024-08-02 21:49:36 +0530

    Animation added

--- sign-in.js
+++ sign-in.js
@@ -114,3 +114,17 @@ function validatePassword(password) {
 function validateField(field) {
   return field != null && field.length > 0;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const signUpButton = document.getElementById('signUp');
+  const signInButton = document.getElementById('signIn');
+  const container = document.getElementById('container');
+
+  signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+  });
+
+  signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+  });
+});
